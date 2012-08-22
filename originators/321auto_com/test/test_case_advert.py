2b9502fc66654bf30ffcc5ee16ef1b3c14dae1f9
ada <andrei.danciuc@asta-s.eu>
2012-08-22 08:20:51 +0300

    addes

---
+++ originators/321auto_com/test/test_case_advert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from ciur.common.DomParserFile import DomParserFile
+from ciur.util.AdvancedDict    import AdvancedDict
+
+class AdvertTestCase(unittest.TestCase):
+    def runTest(self):
+        dpf = DomParserFile(
+            name = "test",
+            source = "../xjsons/advert.json"
+        )
+
+        expect = AdvancedDict()
+        expect.load_json("./expect/advert_01.json")
+
+        xpath = dpf.get_version()
+        dpf.validate_configs(xpath)
+        with open("../page_samples/advert_01.html") as f: html = f.read()
+        html = html.decode("iso-8859-1")
+        got = dpf.dive_html_root_level(html = html, disable_br=False)
+
+        print got.get_pretty()
+        self.assertEqual(expect, got)
