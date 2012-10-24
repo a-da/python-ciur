7f9d0898891545968d9009cdff6f11d78ea2cfbb
ada <andrei.danciuc@asta-s.eu>
2012-10-24 15:26:54 +0300

    draft

---
+++ originators/banks/fincombank_com/test/fincombank_test_case.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import unittest
+
+from advanced_dict import AdvancedDict
+
+from ciur.common.DomParserFile import DomParserFile
+
+
+class EximbankTestCase(unittest.TestCase):
+    def runTest(self):
+        os.chdir(os.path.dirname(__file__))
+
+        dpf = DomParserFile(
+            name   = "test",
+            source = "../xjsons/fincombank.json"
+        )
+        for i in [2]:
+            os.chdir(os.path.dirname(__file__))
+
+            expect = AdvancedDict()
+            expect.load_json("./expect/fincombank_%02d.json" %i)
+            expect = AdvancedDict.pretty_float(expect)
+
+            xpath = dpf.get_version()
+            dpf.validate_configs(xpath)
+            with open("../page_samples/fincombank_%02d.html" %i) as f: html = f.read()
+
+            got = dpf.dive_html_root_level(html = html)
+            print got.get_pretty()
+
+            self.assertEqual(expect.comparable(), got.comparable())
+
