8c12f7d19176ff39cb24c46f0a2c0df0b2b031b6
ada <andrei.danciuc@asta-s.eu>
2012-10-24 15:25:08 +0300

    draft

---
+++ originators/banks/comertbank_md/test/comertbank_test_case.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+import unittest
+
+from advanced_dict import AdvancedDict
+
+from ciur.common.DomParserFile import DomParserFile
+
+
+class ComertbankTestCase(unittest.TestCase):
+    def runTest(self):
+        os.chdir(os.path.dirname(__file__))
+
+        dpf = DomParserFile(
+            name = "test",
+            source = "../xjsons/comertbank.json"
+        )
+        for i in [1]:
+            expect = AdvancedDict()
+            expect.load_json("./expect/comertbank_%02d.json" %i)
+            expect = AdvancedDict.pretty_float(expect)
+
+            xpath = dpf.get_version()
+            dpf.validate_configs(xpath)
+            with open("../page_samples/comertbank_%02d.html" %i) as f: html = f.read()
+
+            got = dpf.dive_html_root_level(html = html)
+            print got.get_pretty()
+
+            self.assertEqual(expect.comparable(), got.comparable())
+
+
