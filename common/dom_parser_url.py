bfd722c4733109b760bf5652a7c630187ff36871
ada <andrei.danciuc@asta-s.eu>
2012-10-24 07:11:56 +0300

    draft

---
+++ common/dom_parser_url.py
@@ -0,0 +1,10 @@
+from ciur.common import DomParser
+
+class DomParserUrl(DomParser):
+    """
+    Remote implimentation of DomParser
+    1. Got xjson from links
+    2. Got page from link
+    3. return desired data form parsed page into json format
+    """
+    pass
