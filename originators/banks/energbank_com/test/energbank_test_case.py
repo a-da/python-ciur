5421d02f76fa247b12d62dd09bc48ecd0711afb3
ada <andrei.danciuc@asta-s.eu>
2012-10-24 15:26:01 +0300

    draft

---
+++ originators/banks/energbank_com/test/energbank_test_case.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import unittest
+
+from advanced_dict import AdvancedDict
+
+from ciur.common.DomParserFile import DomParserFile
+
+class EnergbankTestCase(unittest.TestCase):
+    def runTest(self):
+        os.chdir(os.path.dirname(__file__))
+
+        dpf = DomParserFile(
+            name = "test",
+            source = "../xjsons/energbank.json"
+        )
+        for i in [1]:
+            expect = AdvancedDict()
+            expect.load_json("./expect/energbank_%02d.json" %i)
+            expect = AdvancedDict.pretty_float(expect)
+
+            xpath = dpf.get_version()
+            dpf.validate_configs(xpath)
+            with open("../page_samples/energbank_%02d.html" %i) as f: html = f.read()
+
+            got = dpf.dive_html_root_level(html = html)
+            print got.get_pretty()
+
+            self.assertEqual(expect.comparable(), got.comparable())
+
