a071dd4f7093514334f1055eed5200ff7b392e38
ada <andrei.danciuc@asta-s.eu>
2012-10-24 07:10:59 +0300

    draft

---
+++ common/dom_parser_mysql.py
@@ -0,0 +1,11 @@
+from ciur.common import DomParser
+
+class DomParserMySQL(DomParser):
+    """
+    Database MySQL implimentation of DomParser
+    1. Got xjson from Database MySQL
+    2. Got page *
+    3. return desired data form parsed page into json format
+    """
+    pass
+
