914276603f29de0162a1c544c36c15dde37e4d3b
ada <ada@asta-s.eu>
2013-11-11 22:53:28 +0200

    add HttpRaiseException

--- common/__init__.py
+++ common/__init__.py
@@ -3,6 +3,7 @@ from common import json_dump
 from common import str_startswith
 from inline_handlers import InlineHandlers
 from inline_handlers import InlineHandlersException
+from inline_handlers import HttpRaiseException
 from dom_parser import DomParser
 from dom_parser import DomParserException
 from dom_parser_file import DomParserFile
