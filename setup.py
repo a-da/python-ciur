f6c0db593719f253288fe69279f7cbe4520767b6
ada <ada@asta-s.eu>
2014-10-25 21:40:58 +0300

    ciur-0.1.1dev:
      - install bug fix

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 from setuptools import setup
 from pip.req import parse_requirements
-from __init__ import VERSION
+
+name = "ciur"
+version = __import__(name).VERSION
 
 setup(
-    name="ciur",
-    version=VERSION,
+    name=name,
+    version=version,
     url='http://asta-s.eu',
     dependency_links=[
        "git+https://bitbucket.org/ada/ciur.git#egg=ciur"
