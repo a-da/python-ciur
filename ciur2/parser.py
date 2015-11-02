47e5bf51a74ab6f369af17c4ad20052b5a2d14ab
ada <andrei.danciuc@asta-s.eu>
2015-11-02 18:03:17 +0200

    continue work on parser

--- ciur2/parser.py
+++ ciur2/parser.py
@@ -7,19 +7,42 @@ root /jobs/job +
     location . *
         country ./country str +1
         city ./city str +1
-        zip ./postalcode ["str", "*1"]
+        zip ./postalcode str *1
 """
+
+bnf = {
+    "xpath": "(\.?(?:/\w+)*)",
+    "type_list": "((?:str|int) )*[\+\*]\d+?)"
+}
 
 
 def parse(data):
-    for number, line in enumerate(data.splitlines(), 1):
-        line = line.strip()
-        print ">_%s_" % line
-        if not line:
-            pass  # ignore
+    def _line(string, tab, parent):
+        print "[INFO] string `%s`" % string
+        if string == "":
+            print "[INFO] ignore"
         else:
-            m = re.search("^(\w+) ([\./a-zA-Z]+)$", line)
-            if m:
-                print "match"
+            # if start with indent then this is child
+
+            pattern = "^%s([a-z]+) " % tab
+
+            m = re.search(pattern, string)
+            if not m:
+                print string
+                print "[ERROR] expect label on %s, see pattern `%s`" % ((number, 0), pattern)
+                exit(1)
+            name = m.group(1)
+
+            return {
+                "name": name,
+            }
+
+    rule = {}
+    ret = None
+    tab_ = ""
+    for number, line in enumerate(data.splitlines(), 1):
+        ret = _line(line, parent=ret, tab=tab_)
+        rule["name"] = rule
+
 
 parse(input_data)
